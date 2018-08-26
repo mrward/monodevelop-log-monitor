dca1936308ec91c935b1b8656057f43ca2f8bbe1
Matt Ward <ward.matt@gmail.com>
2018-08-26 13:05:40 +0100

    Add button to IDE Log Viewer pad to open current IDE log
    Opens the IDE log into the text editor.

--- src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/CurrentIdeLogFile.cs
+++ src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/CurrentIdeLogFile.cs
@@ -1,5 +1,5 @@
 ﻿//
-// LogMonitorPad.cs
+// CurrentIdeLogFile.cs
 //
 // Author:
 //       Matt Ward <matt.ward@microsoft.com>
@@ -25,44 +25,25 @@
 // THE SOFTWARE.
 
 using System;
-using Gtk;
-using MonoDevelop.Components;
-using MonoDevelop.Components.Docking;
 using MonoDevelop.Core;
+using MonoDevelop.Ide;
 using MonoDevelop.Ide.Gui;
 
-namespace MonoDevelop.LogMonitor.Gui
+namespace MonoDevelop.LogMonitor
 {
-	class LogMonitorPad : PadContent
+	static class CurrentIdeLogFile
 	{
-		LogMonitorWidget widget;
-		Control control;
-
-		public override Control Control {
-			get {
-				if (control == null) {
-					widget = new LogMonitorWidget ();
-					control = widget.ToGtkWidget ();
-				}
-				return control;
-			}
+		static CurrentIdeLogFile ()
+		{
+			FileName = UserProfile.Current.LogDir.Combine ("Ide.log");
 		}
 
-		protected override void Initialize (IPadWindow window)
+		public static FilePath FileName { get; }
+
+		public static void Open ()
 		{
-			var toolbar = window.GetToolbar (DockPositionType.Right);
-
-			var openIdeLogButton = new Button (new ImageView (Ide.Gui.Stock.TextFileIcon, IconSize.Menu));
-			openIdeLogButton.Clicked += OnOpenIdeLogButtonClick;
-			openIdeLogButton.TooltipText = GettextCatalog.GetString ("Open IDE log");
-			toolbar.Add (openIdeLogButton);
-
-			toolbar.ShowAll ();
-		}
-
-		void OnOpenIdeLogButtonClick (object sender, EventArgs e)
-		{
-			CurrentIdeLogFile.Open ();
+			var fileInfo = new FileOpenInformation (FileName);
+			IdeApp.Workbench.OpenDocument (fileInfo).Ignore ();
 		}
 	}
 }
