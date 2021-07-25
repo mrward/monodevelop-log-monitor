74c9d7a0855e0481797b5ea3405f32b38e07130f
Matt Ward <matt.ward@microsoft.com>
2021-07-25 12:01:45 +0100

    Update addin version to 0.3

--- src/MonoDevelop.LogMonitor/Properties/AddinInfo.cs
+++ src/MonoDevelop.LogMonitor/Properties/AddinInfo.cs
@@ -28,7 +28,7 @@ using Mono.Addins;
 [assembly: Addin (
 	"LogMonitor",
 	Namespace = "MonoDevelop",
-	Version = "0.2",
+	Version = "0.3",
 	Category = "IDE extensions")]
 
 [assembly: AddinName ("Log Monitor")]
