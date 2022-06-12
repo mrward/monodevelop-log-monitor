1b94bf55f86fb2a588aa3d94730dbb70c5a03101
Matt Ward <matt.ward@microsoft.com>
2022-06-12 12:28:03 +0100

    Remove status bar icon code
    VS Mac 17.x does not support this so remove it.

--- src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/LogMonitorLogger.cs
+++ src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/LogMonitorLogger.cs
@@ -24,20 +24,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using System;
-using MonoDevelop.Core;
 using MonoDevelop.Core.Logging;
-using MonoDevelop.Ide;
-using MonoDevelop.Ide.Gui;
-using MonoDevelop.LogMonitor.Gui;
 
 namespace MonoDevelop.LogMonitor
 {
 	public class LogMonitorLogger : ILogger
 	{
-		StatusBarIcon statusBarIcon;
-		int errorsCount;
-
 		public EnabledLoggingLevel EnabledLevel => EnabledLoggingLevel.UpToWarn;
 
 		public string Name => nameof (LogMonitorLogger);
@@ -45,54 +37,6 @@ namespace MonoDevelop.LogMonitor
 		public void Log (LogLevel level, string message)
 		{
 			LogMonitorMessages.ReportLogMessage (level, message);
-
-			switch (level) {
-				case LogLevel.Error:
-					OnLogError (message);
-					break;
-				case LogLevel.Fatal:
-					OnLogFatal (message);
-					break;
-				case LogLevel.Warn:
-					OnLogWarn (message);
-					break;
-			}
-		}
-
-		void OnLogWarn (string message)
-		{
-
-		}
-
-		void OnLogFatal (string message)
-		{
-			errorsCount++;
-		}
-
-		void OnLogError (string message)
-		{
-			errorsCount++;
-			ShowStatusIcon ();
-		}
-
-		void ShowStatusIcon ()
-		{
-			Runtime.RunInMainThread (() => {
-				if (statusBarIcon == null) {
-					var icon = ImageService.GetIcon (Stock.TextFileIcon, Xwt.IconSize.Small);
-					statusBarIcon = IdeApp.Workbench.StatusBar.ShowStatusIcon (icon);
-					statusBarIcon.Clicked += StatusBarIconClicked;
-				}
-				statusBarIcon.Title = GettextCatalog.GetString ("IDE log errors");
-				statusBarIcon.ToolTip = GettextCatalog.GetPluralString ("{0} IDE log error", "{0} IDE log errors", errorsCount, errorsCount);
-				statusBarIcon.SetAlertMode (1);
-			}).Ignore ();
-		}
-
-		void StatusBarIconClicked (object sender, StatusBarIconClickedEventArgs e)
-		{
-			Pad pad = IdeApp.Workbench.GetPad<LogMonitorPad> ();
-			pad.BringToFront (true);
 		}
 	}
 }
